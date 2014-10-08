1198432ad65dbc482d2624ff14fbc4de4b770f53
helenakristina <hklucia@gmail.com>
2014-10-08 14:57:04 -0700

    Update parse-text.py
    added parse-text shell functions

--- parse-text.py
+++ parse-text.py
@@ -1,1 +1,15 @@
-#code
+def parse_text(txt, source, hashtag=""):
+    if source=="SMS" and hashtag=="":
+        return generate_list(text, smslength)
+    else if source=="TWITTER":
+        tweetlen = 140-len(hashtag+1)--one char for #
+        tweetlist = generate_list(text, tweetlen)
+        return tweetlist
+    else:
+        return "ERROR"
+        
+def generate_list(text, chunk_len):
+    textlist=[]
+    
+    return textlist
+
